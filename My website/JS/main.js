6e40f39a000f604fc40db2baf9c1598435c0eee5
Phillip Boswell <phillip.boswell@gmail.com>
2022-06-09 12:34:51 -0700

    Not working but making changes
    Want to change the Contact form info page a bit

--- My website/JS/main.js
+++ My website/JS/main.js
@@ -1,28 +1,29 @@
 function validateForm() {
-    let a = document.forms["myForm"]["Name"].value;
-    let b = document.forms ["myForm"]["Email"].value;
-    let c = document.forms ["myForm"]["Phone"].value;
+    const form = document.querySelector('.contact_form')
+    const name = document.forms["myForm"]["Name"].value;
+    let email = document.forms ["myForm"]["Email"].value;
+    let msg = document.forms ["myForm"]["Phone"].value;
 
-    if(a==""){
+    if(name==""){
         let FnameReturn = "please fill in your name";
         document.getElementById("NameError").innerHTML = FnameReturn.fontcolor("48BBBB");
         
     }
 
-     if(b==""){
+     if(email==""){
         let LnameReturn = "please fill in your email"
         document.getElementById("EmailError").innerHTML = LnameReturn.fontcolor("48BBBB");
         
     }
  
 
-    if(c==""){
+    if(msg==""){
         let LnameReturn = "please fill in your phone number"
         document.getElementById("PhoneError").innerHTML = LnameReturn.fontcolor("48BBBB");
         
     }
 
-    if(a=="" || b=="" || c==""){
+    if(name=="" || email=="" || msg==""){
         return false;
     }
 }
@@ -34,6 +35,22 @@ function openForm(){
 function closeForm(){
     document.getElementById("myFormcontainer").style.display = "none";
 }
+//function to send email.
+function sendMesg(e){
+    e.preventDefault();
+
+    Email.send({
+        SecureToken : "b8ff57e3-8e14-48ee-93b7-c16a5be89c6c",
+        To : 'phillip.boswell.job@gmail.com',
+        From : email.value,
+        Subject : "Job Email",
+        Body : msg.value
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener('submit', sendMesg);
 
 $("#slideshow > div:gt(0)").hide();
 
